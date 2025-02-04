1b1ac1268f88c8978415f13b871d060f9479f68f
prithvikv <kv.prithvi5@gmail.com>
2025-02-04 12:59:39 -0800

    Added list of items

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+
+function ListGroup() {
+  const items = ["New York", "San Francisco", "Los Vegas", "Los Angeles"];
+  return (
+    <Fragment>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+}
+
+export default ListGroup;
