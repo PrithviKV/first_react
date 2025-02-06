be69afa69f3a251be54dab0af2960b40a1ff5298
prithvikv <kv.prithvi5@gmail.com>
2025-02-06 05:30:46 -0800

    Add highlight selected item function

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,30 @@
-import { Fragment } from "react";
+import { useState } from "react";
 
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Los Vegas", "Los Angeles"];
+  let items = ["New York", "San Francisco", "Los Vegas", "Los Angeles"];
+
+  const [selectedindex, setSelectedIndex] = useState(-1);
+
   return (
-    <Fragment>
+    <>
       <h1>List</h1>
+      {items.length === 0 && <p>No List Items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedindex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
-    </Fragment>
+    </>
   );
 }
 
